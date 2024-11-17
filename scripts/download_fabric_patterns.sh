66a96af2a8bede88cf1a76044eefa4b98003935a
xvnpw <17719543+xvnpw@users.noreply.github.com>
2024-11-17 08:45:39 +0100

    workflow to update patterns

--- scripts/download_fabric_patterns.sh
+++ scripts/download_fabric_patterns.sh
@@ -4,6 +4,8 @@ git clone https://github.com/danielmiessler/fabric $GIT_FABRIC_DIR
 
 rm -rf prompts/fabric_patterns/*
 
+rm -rf $GIT_FABRIC_DIR/patterns/raycast
+
 cp -r $GIT_FABRIC_DIR/patterns/* prompts/fabric_patterns/
 
 rm -rf $GIT_FABRIC_DIR
